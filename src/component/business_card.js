b4a2147516f663226df451981b2e64d711546df3
bvth <thanh.bui1995@gmail.com>
2016-11-14 01:17:18 +0200

    3rd commit

--- src/component/business_card.js
+++ src/component/business_card.js
@@ -6,7 +6,7 @@ export default class Card extends React.Component{
             <div className="bcard">
                 <h2>Email</h2><span>thanh.bui1995@gmail.com</span>
                 <h2>Linkedin</h2><span><a href="https://fi.linkedin.com/in/thanh-bui-29a5a199">https://fi.linkedin.com/in/thanh-bui-29a5a199</a></span>
-                <h2>Github</h2><span><a href="github.com/bvth">github.com/bvth</a></span>
+                <h2>Github</h2><span><a href="https://github.com/bvth">github.com/bvth</a></span>
             </div>
         )
     }
